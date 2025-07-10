a691769f15fe6822434ec9018b6655a4e41bdf9b
Nauman Chaudhary <162007974+numancr7@users.noreply.github.com>
2025-07-10 15:07:13 +0500

    dark theme default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <NextAuthSessionProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             enableSystem={false}
             disableTransitionOnChange
           >
